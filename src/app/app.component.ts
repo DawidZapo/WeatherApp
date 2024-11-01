a3cd40fdc1c687fc904ef36d2d88192f84eca22f
DawidZapo <dawidzapo@gmail.com>
2024-11-01 19:56:29 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgForOf, NgIf} from "@angular/common";
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatIcon} from '@angular/material/icon';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
+import {MatInput} from '@angular/material/input';
+import {MatButton, MatIconButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatLabel, NgIf, MatToolbar, MatIcon, MatFormField, MatCard, MatCardHeader, MatCardContent, MatCardActions, MatCardTitle, MatInput, MatIconButton, NgForOf, MatButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'WeatherApp';
+
+  items = [1];
 }
